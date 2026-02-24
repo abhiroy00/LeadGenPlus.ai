4f85ad9f9c3af0fe3513beef35b53b63a4724311
NamanSharma123 <139445554+NamanSharma123@users.noreply.github.com>
2026-02-24 22:04:16 +0530

    backend structure
    backend structure

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
+    'apps.campaigns',
+    'apps.leads',
+    'apps.inbox',
+    'apps.analytics',
+    'apps.agents',
 ]
 
 MIDDLEWARE = [
